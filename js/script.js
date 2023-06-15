c6249277d7bd2f7d82b8156c2fbad335e60cd489
risa_hamawaki <riiisa@softbank.ne.jp>
2023-06-16 01:06:06 +0900

    Display all members information to HTML, fix layout.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,33 @@
-var api_url = 'https://script.google.com/macros/s/AKfycbxjt3at4c_4Mqb_ykUmUxtCBM6kuTiJLP2hKAe4DOC1LOCh18l6603AIgnjl55sH5EK/exec'; //生成したAPIのURLを指定
+var api_url = 'https://script.google.com/macros/s/AKfycbxjt3at4c_4Mqb_ykUmUxtCBM6kuTiJLP2hKAe4DOC1LOCh18l6603AIgnjl55sH5EK/exec'; 
 
-$.getJSON(api_url, function(json) {
-  $.each(json, function(i, item) {
-    if (i === 0) {
-      $("#id-firstline").text(item.id);
-      $("#name-firstline").text(item.name);
-      $("#furigana-firstline").text(item.furigana);
-      $("#gender-firstline").text(getGenderText(item.gender));
-      $("#tel-firstline").text(item.tel);
-      $("#birthday-firstline").text(birthdayToString(item.birthday));
-      $("#status-firstline").text(getStatusText(item.status));
-    } else if (i === 1) {
-      $("#id-secondline").text(item.id);
-      $("#name-secondline").text(item.name);
-      $("#furigana-secondline").text(item.furigana);
-      $("#gender-secondline").text(getGenderText(item.gender));
-      $("#tel-secondline").text(item.tel);
-      $("#birthday-secondline").text(birthdayToString(item.birthday));
-      $("#status-secondline").text(getStatusText(item.status));
-    }
-  });
+fetch(api_url)
+.then(function (fetch_data) {
+return fetch_data.json();
+})
+.then(function (json) {
+for (var i in json) {
+    let membersId = json[i].id;
+    let membersName = json[i].name;
+    let membersFurigana = json[i].furigana;
+    let membersGender = getGenderText(json[i].gender);
+    let membersTel = json[i].tel;
+    let membersBirthday = birthdayToString(json[i].birthday);
+    let membersStatus = getStatusText(json[i].status);
+    
+    let $divsLine = $('<tr id="line" class="line">');
+    $divsLine.append($('<td>' + membersId + '</td>'));
+    $divsLine.append($('<td>' + membersName + '<img src="./img/iconmonstr-external-link-thin-240.png" alt="詳細へのリンク"> </th> </td>'));
+    $divsLine.append($('<td>' + membersFurigana + '</td>'));
+    $divsLine.append($('<td>' + membersGender + '</td>'));
+    $divsLine.append($('<td>' + membersTel + '</td>'));
+    $divsLine.append($('<td>' + membersBirthday + '</td>'));
+    $divsLine.append($('<td>' + membersStatus + '</td>'));
+    $divsLine.append($('<td>4</td>'));
+    $divsLine.append($('<td><div class="flex"><div class="button-date"><a href="#" class="text-medium">来店登録</a></div><div class="button-history"><a href="#" class="text-medium">来店履歴</a></div></div></td>'));
+
+    $divs.append($('</tr>'));
+    $("#members-table").append($divsLine);
+  }
 });
 
 function getGenderText(genderValue) {
